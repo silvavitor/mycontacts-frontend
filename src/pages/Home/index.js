dd92023193a234c6d317ba3dbe60b937615e5e71
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-03-01 00:43:44 -0300

    refac: use contacts list as home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,92 @@
+import {
+  Container, InputSearchContainer, Header, ListContainer, Card,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
 export default function Home() {
-  return 'Home page';
+  return (
+    <Container>
+
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquise pelo nome..." />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button" className="sort-button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-title">
+              <strong>Vitor Silva</strong>
+              <small>instagram</small>
+            </div>
+            <span>vitor@dev.com.br</span>
+            <span>(51) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Trash" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-title">
+              <strong>Vitor Silva</strong>
+              <small>instagram</small>
+            </div>
+            <span>vitor@dev.com.br</span>
+            <span>(51) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Trash" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-title">
+              <strong>Vitor Silva</strong>
+              <small>instagram</small>
+            </div>
+            <span>vitor@dev.com.br</span>
+            <span>(51) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Trash" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
 }
