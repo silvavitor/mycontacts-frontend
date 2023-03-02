b7c81c8a14167c6d75978c0a8054af4c2d940a4b
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-03-02 00:21:22 -0300

    feat: create input components

---
+++ src/components/Input.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export default styled.input`
+  padding: 0 16px;
+  width: 100%;
+  height: 52px;
+  background: #fff;
+  border: 2px solid #fff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  border-radius: 4px;
+  outline: none;
+  font-size: 16px;
+  transition: border-color 0.2s ease-in;
+
+  &:focus  {
+    border-color: ${({ theme }) => theme.colors.primary.main};
+  }
+`;
