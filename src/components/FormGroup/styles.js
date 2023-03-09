e6daeea65cde15d43de3627262eca042c0df6631
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-03-09 00:34:46 -0300

    feat: add error prop on form group

--- src/components/FormGroup/styles.js
+++ src/components/FormGroup/styles.js
@@ -4,4 +4,11 @@ export const Container = styled.div`
   & + & {
     margin-top: 16px;
   }
+
+  small {
+    margin-top: 8px;
+    font-size: 12px;
+    display:block;
+    color: ${({ theme }) => theme.colors.danger.main};
+  }
 `;
