c202f903b0081cf20b4e3a1aa0bee86e2ade52d1
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-03-08 23:34:14 -0300

    refac: abstract button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export default styled.button`
-  width: 100%;
   height: 52px;
+  padding: 0 16px;
   border: none;
   background: ${({ theme }) => theme.colors.primary.main};
   color: #fff;
@@ -24,4 +24,15 @@ export default styled.button`
     background: #ccc;
     cursor: default;
   }
+
+  ${({ theme, danger }) => danger && css`
+    background: ${theme.colors.danger.main};
+    &:hover {
+      background: ${theme.colors.danger.light};
+    }
+
+    &:active {
+      background: ${theme.colors.danger.dark};
+    }
+  `}
 `;
