942bdc1184324820e5c508bc82fd89b28b38345e
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-03-08 23:34:47 -0300

    feat: create modal

---
+++ src/components/Modal/index.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+
+import { Overlay, Container, Footer } from './styles';
+
+import Button from '../Button';
+
+export default function Modal({ danger }) {
+  return ReactDOM.createPortal(
+    <Overlay>
+      <Container danger={danger}>
+        <h1> Titulo </h1>
+        <p>Corpo</p>
+        <Footer>
+          <button type="button" className="cancel-button">
+            Cancelar
+          </button>
+
+          <Button type="button" danger={danger}>
+            Deletar
+          </Button>
+        </Footer>
+      </Container>
+    </Overlay>,
+    document.getElementById('modal-root'),
+  );
+}
+
+Modal.propTypes = {
+  danger: PropTypes.bool,
+};
+
+Modal.defaultProps = {
+  danger: false,
+};
