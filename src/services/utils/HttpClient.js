78bc4807cfe228f955e2cd38350d231f03b1b158
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-05-20 17:10:28 -0300

    feat: use APIError class

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -1,3 +1,5 @@
+import APIError from '../../errors/APIError';
+
 class HttpClient {
   constructor(baseUrl) {
     this.baseUrl = baseUrl;
@@ -18,7 +20,7 @@ class HttpClient {
       return body;
     }
 
-    throw new Error(body?.error || `${response.status} - ${response.statusText}`);
+    throw new APIError(response, body);
   }
 }
 
