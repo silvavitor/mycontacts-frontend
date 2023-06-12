429811912cd8179d45691ab7fa82da8032de4545
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-06-12 10:47:18 -0300

    feat: add post method to HttpClient

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -22,6 +22,31 @@ class HttpClient {
 
     throw new APIError(response, body);
   }
+
+  async post(path, body) {
+    const headers = new Headers({
+      'Content-Type': 'application/json',
+    });
+
+    const response = await fetch(this.baseUrl + path, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers,
+    });
+
+    let responseBody = null;
+    const contentType = response.headers.get('Content-Type');
+
+    if (contentType.includes('application/json')) {
+      responseBody = await response.json();
+    }
+
+    if (response.ok) {
+      return responseBody;
+    }
+
+    throw new APIError(response, body);
+  }
 }
 
 export default HttpClient;
