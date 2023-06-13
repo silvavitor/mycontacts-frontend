1c8930a680fdaeb913d048a7e5328519774018d4
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-06-13 17:42:11 -0300

    refac: clean new contact fields after create

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -31,6 +31,12 @@ const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
       setPhone(formatPhone(contact.phone ?? ''));
       setCategoryId(contact.category_id ?? '');
     },
+    resetFields: () => {
+      setName('');
+      setEmail('');
+      setPhone('');
+      setCategoryId('');
+    },
   }));
 
   const {
