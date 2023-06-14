a184546c76880e7cfa46f4b34201dad388866541
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-06-14 01:29:31 -0300

    feat: create useSafeAsyncAction hook

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,39 +1,44 @@
 import { useParams, useHistory } from 'react-router-dom';
-import { useEffect, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
 import ContactsService from '../../services/ContactsService';
 import Loader from '../../components/Loader';
 import toast from '../../utils/toast';
-import useSafeAsyncState from '../../hooks/useSafeAsyncState';
+import useSafeAsyncAction from '../../hooks/useSafeAsyncAction';
 
 export default function EditContact() {
-  const [isLoading, setIsLoading] = useSafeAsyncState(true);
-  const [contactName, setContactName] = useSafeAsyncState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [contactName, setContactName] = useState('');
 
   const contactFormRef = useRef(null);
   const { id } = useParams();
   const history = useHistory();
+  const safeAsyncAction = useSafeAsyncAction();
 
   useEffect(() => {
     async function loadContact() {
       try {
         const contact = await ContactsService.getContactById(id);
-        contactFormRef.current.setFieldsValues(contact);
 
-        setContactName(contact.name);
-        setIsLoading(false);
+        safeAsyncAction(() => {
+          contactFormRef.current.setFieldsValues(contact);
+          setContactName(contact.name);
+          setIsLoading(false);
+        });
       } catch (error) {
-        history.push('/');
-        toast({
-          type: 'danger',
-          text: 'Contato não encontrado',
+        safeAsyncAction(() => {
+          history.push('/');
+          toast({
+            type: 'danger',
+            text: 'Contato não encontrado',
+          });
         });
       }
     }
 
     loadContact();
-  }, [id, history, setContactName, setIsLoading]);
+  }, [id, history, safeAsyncAction]);
 
   async function handleSubmit(formData) {
     try {
