e91c00f3496a15d230dce8eb0e294a15094d0868
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-06-15 14:00:58 -0300

    feat: add category mapper

--- src/services/CategoriesService.js
+++ src/services/CategoriesService.js
@@ -1,3 +1,4 @@
+import CategoryMapper from './mappers/CategoryMapper';
 import HttpClient from './utils/HttpClient';
 
 class ContegoriesService {
@@ -5,8 +6,9 @@ class ContegoriesService {
     this.apiClient = new HttpClient('http://localhost:3132');
   }
 
-  listCategories() {
-    return this.apiClient.get('/categories');
+  async listCategories() {
+    const categories = await this.apiClient.get('/categories');
+    return categories.map(CategoryMapper.toDomain);
   }
 }
 
