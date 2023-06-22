6b0f500fd9519880c51bda2bffa070b3344df9e1
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-06-22 19:01:10 -0300

    refac: separate logic from ui code

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,61 +1,15 @@
-import { useParams, useHistory } from 'react-router-dom';
-import { useEffect, useState, useRef } from 'react';
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
-import ContactsService from '../../services/ContactsService';
 import Loader from '../../components/Loader';
-import toast from '../../utils/toast';
-import useSafeAsyncAction from '../../hooks/useSafeAsyncAction';
+import useEditContact from './useEditContact';
 
 export default function EditContact() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [contactName, setContactName] = useState('');
-
-  const contactFormRef = useRef(null);
-  const { id } = useParams();
-  const history = useHistory();
-  const safeAsyncAction = useSafeAsyncAction();
-
-  useEffect(() => {
-    async function loadContact() {
-      try {
-        const contact = await ContactsService.getContactById(id);
-
-        safeAsyncAction(() => {
-          contactFormRef.current.setFieldsValues(contact);
-          setContactName(contact.name);
-          setIsLoading(false);
-        });
-      } catch (error) {
-        safeAsyncAction(() => {
-          history.push('/');
-          toast({
-            type: 'danger',
-            text: 'Contato não encontrado',
-          });
-        });
-      }
-    }
-
-    loadContact();
-  }, [id, history, safeAsyncAction]);
-
-  async function handleSubmit(contact) {
-    try {
-      const updatedContact = await ContactsService.updateContact(id, contact);
-      setContactName(updatedContact.name);
-
-      toast({
-        type: 'success',
-        text: 'Contato editado com sucesso!',
-      });
-    } catch (error) {
-      toast({
-        type: 'danger',
-        text: 'Ocorreu um erro ao editar o contato',
-      });
-    }
-  }
+  const {
+    isLoading,
+    contactName,
+    contactFormRef,
+    handleSubmit,
+  } = useEditContact();
 
   return (
     <>
