1e6782add60462a195603c4c6407ae1e0d42faae
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-06-23 13:11:51 -0300

    refac: split ToastContainer logic from ui code

---
+++ src/components/Toast/ToastContainer/useToastContainer.js
@@ -0,0 +1,33 @@
+import { useState, useEffect, useCallback } from 'react';
+import { toastEventManager } from '../../../utils/toast';
+
+export default function useToastContainer() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    function handleAddToast({ type, text, duration }) {
+      setMessages((prevState) => [
+        ...prevState,
+        {
+          id: Math.random(), type, text, duration,
+        },
+      ]);
+    }
+
+    toastEventManager.on('addtoast', handleAddToast);
+
+    return () => {
+      toastEventManager.removeListener('addtoast', handleAddToast);
+    };
+  }, []);
+
+  const handleRemoveMessage = useCallback((id) => {
+    setMessages((prevState) => prevState.filter(
+      (message) => message.id !== id,
+    ));
+  }, []);
+  return {
+    messages,
+    handleRemoveMessage,
+  };
+}
