f65cb8d398a12be125cbdbb58794187d9db97f4d
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-06-25 18:42:24 -0300

    feat: create ListHeader component

---
+++ src/components/ListHeader/index.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import arrow from '../../assets/images/icons/arrow.svg';
+import { Header } from './styles';
+
+export default function ListHeader({ orderBy, onToggleOrderBy }) {
+  return (
+    <Header orderBy={orderBy}>
+      <button type="button" onClick={onToggleOrderBy}>
+        <span>Nome</span>
+        <img src={arrow} alt="Arrow" />
+      </button>
+    </Header>
+  );
+}
+ListHeader.propTypes = {
+  orderBy: PropTypes.string.isRequired,
+  onToggleOrderBy: PropTypes.func.isRequired,
+};
---
+++ src/components/ListHeader/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  margin-top: 24px;
+  margin-bottom: 8px;
+
+  button {
+    background: transparent;
+    border: none;
+    display: flex;
+    align-items: center;
+
+    span {
+      margin-right: 8px;
+      font-weight: bold;
+      color: ${({ theme }) => theme.colors.primary.main};
+    }
+
+    img {
+      transform: ${({ orderBy }) => (orderBy === 'asc' ? 'rotate(180deg)' : 'rotate(0deg)')};
+      transition: transform 0.2s ease-in
+    }
+  }
+`;
