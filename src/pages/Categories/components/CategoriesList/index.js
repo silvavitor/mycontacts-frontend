ec5e2995307c2c03dc2ea490c6460998baf1f246
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-06-25 19:28:05 -0300

    feat: create CategoriesList  component

---
+++ src/pages/Categories/components/CategoriesList/index.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import ListHeader from '../../../../components/ListHeader';
+import Card from '../Card';
+
+export default function CategoriesList({
+  categories, orderBy, onToggleOrderBy, onDeleteCategory,
+}) {
+  return (
+    <>
+      <ListHeader orderBy={orderBy} onToggleOrderBy={onToggleOrderBy} />
+
+      {categories.map((category) => (
+        <Card key={category.id} category={category} onDeleteCategory={onDeleteCategory} />
+      ))}
+    </>
+  );
+}
+
+CategoriesList.propTypes = {
+  categories: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+  })).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  onToggleOrderBy: PropTypes.func.isRequired,
+  onDeleteCategory: PropTypes.func.isRequired,
+};
