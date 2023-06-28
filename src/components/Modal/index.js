896fad3833988206316ba52830b7d0d5817c1678
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-06-28 20:44:54 -0300

    feat: animate modal unmount

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -4,18 +4,21 @@ import { Overlay, Container, Footer } from './styles';
 
 import Button from '../Button';
 import ReactPortal from '../ReactPortal';
+import useAnimatedUnmount from '../../hooks/useAnimatedUnmount';
 
 export default function Modal({
   danger, title, children, cancelLabel, confirmLabel, onCancel, onConfirm, visible, isLoading,
 }) {
-  if (!visible) {
+  const { shouldRender, animatedElementRef } = useAnimatedUnmount(visible);
+
+  if (!shouldRender) {
     return null;
   }
 
   return (
     <ReactPortal containerId="modal-root">
-      <Overlay>
-        <Container danger={danger}>
+      <Overlay isLeaving={!visible} ref={animatedElementRef}>
+        <Container danger={danger} isLeaving={!visible}>
           <h1>{title}</h1>
           <div className="modal-body">
             {children}
