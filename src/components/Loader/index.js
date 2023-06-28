7dc7e60866f2221464584592816efe4ef5def8cd
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-06-28 20:49:27 -0300

    feat: animate Loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -3,9 +3,12 @@ import PropTypes from 'prop-types';
 import { Overlay } from './styles';
 import Spinner from '../Spinner';
 import ReactPortal from '../ReactPortal';
+import useAnimatedUnmount from '../../hooks/useAnimatedUnmount';
 
 export default function Loader({ isLoading }) {
-  if (!isLoading) {
+  const { shouldRender, animatedElementRef } = useAnimatedUnmount(isLoading);
+
+  if (!shouldRender) {
     return null;
   }
 
@@ -18,7 +21,7 @@ export default function Loader({ isLoading }) {
 
   return (
     <ReactPortal containerId="loader-root">
-      <Overlay>
+      <Overlay isLeaving={!isLoading} ref={animatedElementRef}>
         <Spinner size={90} />
       </Overlay>
     </ReactPortal>
