cd47373089a664ac780e165618b2d137b63d5a8c
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-10-24 19:28:09 -0300

    feat: update root rendering

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './components/App';
 
-ReactDOM.render(
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
